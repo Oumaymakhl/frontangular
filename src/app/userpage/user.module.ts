d8da9679862d98a648b70b4c6db38ccfa9b7903f
hmidaahlem <ahlemhmida40@gmail.com>
2024-05-21 12:13:49 +0100

    gestion profil

--- src/app/userpage/user.module.ts
+++ src/app/userpage/user.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { UserComponent } from './user.component';
 import { UserRoutes } from './user.routing';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from 'app/shared/API_service/auth.service';
+import { AppComponent } from 'app/app.component';
 
 @NgModule({
     imports: [
         CommonModule,
         RouterModule.forChild(UserRoutes),
-        FormsModule
-    ],
+        ReactiveFormsModule,HttpClientModule,FormsModule,
+
+    ],providers: [AuthService], 
+    bootstrap: [AppComponent],
     declarations: [UserComponent]
 })
 
